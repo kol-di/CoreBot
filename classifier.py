eee1699675ca158a157a03423ec6eb0fe6ed4606
kol-di <60890747+kol-di@users.noreply.github.com>
2022-11-24 23:11:10 +0300

    shared model instance and refactor

--- classifier.py
+++ classifier.py
@@ -18,79 +18,47 @@ DATA_TRANSFORM = transforms.Compose([
     transforms.ToTensor(),
 ])
 
-# DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
 STATE_DICT_PATH = os.path.join(os.getcwd(), 'model_state_dict')
 
 
-# def create_pretrained_model(state_dict_path):
-#     # Load the pretrained model from pytorch
-#     vgg16 = models.vgg16_bn(weights=models.VGG16_BN_Weights.DEFAULT)
-#
-#     # Change model architecture for my needs
-#     num_features = vgg16.classifier[6].in_features
-#     features = list(vgg16.classifier.children())[:-1]  # Remove last layer
-#     features.extend([nn.Linear(num_features, len(CLASS_OPTIONS))])  # Add layer with 4 outputs
-#     vgg16.classifier = nn.Sequential(*features)  # Replace the model classifier
-#
-#     # Load new weights after additional training
-#     vgg16.load_state_dict(torch.load(state_dict_path, map_location=DEVICE))
-#     vgg16 = vgg16.to(DEVICE)
-#
-#     return vgg16
-
-
-# def img_class(label_idx=None):
-#     if label_idx is not None:
-#         if torch.is_tensor(label_idx):
-#             label_idx = label_idx.item()
-#         try:
-#             print(CLASS_OPTIONS[label_idx])
-#         except KeyError:
-#             pass
-#
-#
-# def which_core_are_you(model, img_filename):
-#     model.eval()
-#
-#     img_name = img_filename.split('/')[-1]
-#     img = Image.open(f'playground/{img_name}')
-#     img_tensor = DATA_TRANSFORM(img).unsqueeze(dim=0).to(DEVICE)
-#
-#     with torch.no_grad():
-#         result = model(img_tensor).cpu().data.numpy().argmax()
-#     return img_class(result)
-
-
-# model = create_pretrained_model(STATE_DICT_PATH)
-# which_core_are_you(model, '7498c829aec8669c19702e255513ab82.png')
-
-
-class Model:
+class CoreModel:
     _class_options = None
-    _data_transforms = None
+    _data_transform = None
     _device = None
-    _state_dict_path = None
 
     _instance = None
 
     model = None
 
     def __new__(cls, *args, **kwargs):
+        print('new')
         if cls._instance is None:
-            instance = super().__new__(cls, *args, **kwargs)
-            cls.__init__(instance, *args, **kwargs)
+            print('long new')
+            instance = super().__new__(cls)
             cls._instance = instance
+            cls._class_options = CLASS_OPTIONS
+            cls._data_transform = DATA_TRANSFORM
+            cls._device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         return cls._instance
 
-    def __init__(self, *args, **kwargs):
-        self._class_options = CLASS_OPTIONS
-        self._data_transforms = DATA_TRANSFORM
-        self._state_dict_path = STATE_DICT_PATH
-        self._device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.model = self.create_pretrained_model()
+    def __init__(self, state_dict_path, rebuild=False):
+        print('init')
+        if self.__class__.model is None or rebuild:
+            print('long init')
+            self.__class__.model = self.create_pretrained_model(state_dict_path)
 
-    def create_pretrained_model(self):
+    def __call__(self, img_filename):
+        self.model.eval()
+
+        img_name = img_filename.split('/')[-1]
+        img = Image.open(f'playground/{img_name}')
+        img_tensor = self._data_transform(img).unsqueeze(dim=0).to(self._device)
+
+        with torch.no_grad():
+            result = self.model(img_tensor).cpu().data.numpy().argmax()
+        return self.img_class_(result)
+
+    def create_pretrained_model(self, state_dict_path):
         # Load the pretrained model from pytorch
         vgg16 = models.vgg16_bn(weights=models.VGG16_BN_Weights.DEFAULT)
 
@@ -101,7 +69,7 @@ class Model:
         vgg16.classifier = nn.Sequential(*features)  # Replace the model classifier
 
         # Load new weights after additional training
-        vgg16.load_state_dict(torch.load(self._state_dict_path, map_location=self._device))
+        vgg16.load_state_dict(torch.load(state_dict_path, map_location=self._device))
         vgg16 = vgg16.to(self._device)
 
         return vgg16
@@ -111,18 +79,13 @@ class Model:
             if torch.is_tensor(label_idx):
                 label_idx = label_idx.item()
             try:
-                print(self._class_options[label_idx])
+                return self._class_options[label_idx]
             except KeyError:
                 pass
 
-    def which_core_are_you(self, img_filename):
-        self.model.eval()
 
-        img_name = img_filename.split('/')[-1]
-        img = Image.open(f'playground/{img_name}')
-        img_tensor = DATA_TRANSFORM(img).unsqueeze(dim=0).to(self._device)
 
-        with torch.no_grad():
-            result = self.model(img_tensor).cpu().data.numpy().argmax()
-        return self.img_class_(result)
-
+model1 = CoreModel(STATE_DICT_PATH)
+model2 = CoreModel(STATE_DICT_PATH)
+print(model1 == model2)
+print(model2('7498c829aec8669c19702e255513ab82.png'))
