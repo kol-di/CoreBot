fc81c2329e649874ba2625e56192869b8c5bc0a0
kol-di <60890747+kol-di@users.noreply.github.com>
2022-11-25 18:54:14 +0300

    Connected service operations

--- main.py
+++ main.py
@@ -3,22 +3,24 @@ import asyncio
 from configparser import ConfigParser
 
 from classifier import create_core_model
-from telebot import start_clinent
+from telebot import start_client, client_main_loop
 
 
 async def main():
     config = ConfigParser()
     config.read("config.ini")
     state_dict_path = config['Files']['STATE_DICT_PATH']
-    model, client = await asyncio.gather(
+
+    _, client = await asyncio.gather(
         create_core_model(state_dict_path),
-        start_clinent()
+        start_client()
     )
-    return model, client
+    await client_main_loop(client)
+    return client
 
 
 if __name__ == '__main__':
     logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                         level=logging.WARNING)
 
-    print(asyncio.run(main()))
+    asyncio.run(main())
